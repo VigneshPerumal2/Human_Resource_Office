e2828b524cf14d9c225424672de6fd778207c101
Vignesh P <34863107+VigneshPerumal2@users.noreply.github.com>
2022-10-18 20:46:59 -0400

    Update Person.java
    Model Person Created

--- src/model/Person.java
+++ src/model/Person.java
@@ -9,5 +9,52 @@ package model;
  * @author vigy
  */
 public class Person {
+    private String name;
+    private int age;
+    private String gender;
+    private String emailId;
+    private long phoneNumber;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    
+    
     
 }
